2e7935f43e288e58c0682a6038f878f0cc319709
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2024-02-21 13:00:11 -0500

    Support literal unions (#2)
    * Add support for literal unions
    
    * Fix unknown not compiling to a blob
    
    * Rename discriminators.ts to unions.ts
    
    * Update renamed import
    
    * Simplify discriminated unions type

---
+++ src/unions.ts
@@ -0,0 +1,36 @@
+type IsUnion<T, U = T> = T extends T ? (U extends T ? never : true) : never;
+type FilterNever<T> = { [k in { [k in keyof T]: T[k] extends never ? never : k }[keyof T]]: T[k] };
+
+type IsLiteral<T> = T extends undefined
+	? true
+	: T extends string
+	? string extends T
+		? false
+		: true
+	: T extends number
+	? number extends T
+		? false
+		: true
+	: T extends boolean
+	? true
+	: false;
+
+export type FindDiscriminator<T> = keyof T &
+	keyof (T extends T
+		? FilterNever<{ [k in keyof T]: T[k] extends string ? (string extends T[k] ? never : k) : never }>
+		: never);
+
+export type IsDiscriminableUnion<T> = true extends IsUnion<T>
+	? FindDiscriminator<T> extends never
+		? false
+		: true
+	: false;
+
+// This doesn't check whether T is a union, only that it is comprised of only literal values.
+// The distinction isn't very important for serialization purposes, as we can still optimize a single literal.
+// We also exclude plain `boolean` here as that has a more efficient special case.
+export type IsLiteralUnion<T> = [boolean, NonNullable<T>] extends [NonNullable<T>, boolean]
+	? false
+	: (T extends T ? (T extends undefined ? true : IsLiteral<T> extends true ? true : false) : never) extends true
+	? true
+	: false;
